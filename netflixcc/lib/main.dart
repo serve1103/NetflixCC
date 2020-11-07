abc32572cb31a54ae893da4ac703c74bc43d2d71
serve1103 <wotjq0101@gmail.com>
2020-11-07 17:21:43 +0900

    netflix theme and bottom

--- netflixcc/lib/main.dart
+++ netflixcc/lib/main.dart
@@ -1,5 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:netflixcc/widget/bottom_bar.dart';
 
-void main() {
+void main() => runApp(MyApp());
   //201107 시작
+
+//MyApp의 상태를 관리하는 _MyAppState 클래스
+class MyApp extends StatefulWidget{
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'netflix',
+      theme: ThemeData(
+        brightness: Brightness.dark ,
+        primaryColor: Colors.black ,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            // TODO: NeverScrollableScrollPhysics
+            // 손가락으로 탭이 넘어가는 것을 방지
+            physics: NeverScrollableScrollPhysics(
+            ),
+            children: <Widget>[
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
 }
---
+++ netflixcc/lib/widget/bottom_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.black,
+      child: Container(
+        height: 50,
+        child: TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          indicatorColor: Colors.transparent,
+          tabs: <Widget>[
+            //TODO: 홈
+            Tab(
+              icon: Icon(
+                Icons.home,
+                size: 18,
+              ),
+              child: Text(
+                '홈',
+                style: TextStyle(
+                  fontSize: 9,
+                ),
+              ),
+            ),
+            //TODO: 검색
+            Tab(
+              icon: Icon(
+                Icons.search,
+                size: 18,
+              ),
+              child: Text(
+                '검색',
+                style: TextStyle(
+                  fontSize: 9,
+                ),
+              ),
+            ),
+            //TODO: 저장목록
+            Tab(
+              icon: Icon(
+                Icons.save_alt,
+                size: 18,
+              ),
+              child: Text(
+                '저장한 컨텐츠',
+                style: TextStyle(
+                  fontSize: 9,
+                ),
+              ),
+            ),
+            //TODO: 더보기
+            Tab(
+              icon: Icon(
+                Icons.list,
+                size: 18,
+              ),
+              child: Text(
+                '더보기 ',
+                style: TextStyle(
+                  fontSize: 9,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
