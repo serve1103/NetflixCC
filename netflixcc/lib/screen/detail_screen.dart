50fc49291678fc6fc92b6243fc311d165fca19d3
serve1103 <wotjq0101@gmail.com>
2020-11-14 16:28:13 +0900

    detail view

--- netflixcc/lib/screen/detail_screen.dart
+++ netflixcc/lib/screen/detail_screen.dart
@@ -5,6 +5,7 @@ import 'package:netflixcc/model/model_movie.dart';
 //TODO: 영화의 세부 정보를 보여주는 화면
 class DetailScreen extends StatefulWidget {
   final Movie movie;
+
   DetailScreen({this.movie});
 
   _DetailScreenState createState() => _DetailScreenState();
@@ -15,8 +16,7 @@ class _DetailScreenState extends State<DetailScreen> {
   bool like = false;
 
   @override
-
-  void initState(){
+  void initState() {
     super.initState();
     like = widget.movie.like;
   }
@@ -47,18 +47,66 @@ class _DetailScreenState extends State<DetailScreen> {
                           child: Container(
                             child: Column(
                               children: <Widget>[
+                                //TODO: 상단 이미지 및 배경
                                 Container(
                                   padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
-                                  child: Image.asset('./images/' +widget.movie.poster),
+                                  child: Image.asset(
+                                      './images/' + widget.movie.poster),
                                   height: 300,
                                 ),
+                                //TODO: 영화 정보
                                 Container(
                                   padding: EdgeInsets.all(7),
-                                  child: Text('99% 일치 2019 15+ 시즌1대'),
+                                  child: Text(
+                                    '99% 일치 2019 15+ 시즌 1개',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 13),
+                                  ),
                                 ),
-                                Container(),
-                                Container(),
-                                Container(),
+                                //TODO: 영화 타이틀
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(
+                                    widget.movie.title,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                ),
+                                //TODO: 영화 재생 버튼
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    color: Colors.red,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Icon(Icons.play_arrow),
+                                        Text('재생'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                //TODO: 영화 정보설명
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(widget.movie.toString()),
+                                ),
+                                //TODO: 영화 출연진 정보
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '출연: 현빈, 손예진, 서지혜 \n제작자 : 이정효, 박지은',
+                                    style: TextStyle(
+                                      color: Colors.white60,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -66,17 +114,91 @@ class _DetailScreenState extends State<DetailScreen> {
                       ),
                     ),
                   ),
+                  //TODO: 앱바
+                  Positioned(
+                    child: AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                  ),
                 ],
               ),
-              makeMenuButton(),
+              //TODO: 내가 찜한 컨텐츠
+              Container(
+                color: Colors.black26,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Column(
+                          children: <Widget>[
+                            like ? Icon(Icons.check) : Icon(Icons.add),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                            ),
+                            Text(
+                              '내가 찜한 컨텐츠',
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.white60,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    //TODO: 좋아요 버튼
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.thumb_up_alt),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                            ),
+                            Text(
+                              '평가',
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.white60,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    //TODO: 공유버튼
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.share),
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                            ),
+                            Text(
+                              '공유',
+                              style: TextStyle(
+                                  fontSize: 11,
+                                  color: Colors.white60,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-Widget makeMenuButton() {
-  return Container();
 }
