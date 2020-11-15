6abf1db8c1505c1bd21302108b77845a9641ed70
serve1103 <wotjq0101@gmail.com>
2020-11-15 16:05:31 +0900

    detail view finish

--- netflixcc/lib/widget/box_slider.dart
+++ netflixcc/lib/widget/box_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflixcc/model/model_movie.dart';
+import 'package:netflixcc/screen/detail_screen.dart';
 
 class BoxSlider extends StatelessWidget {
   //TODO: 홈스크린에서 영화정보 가져오기
@@ -20,7 +21,7 @@ class BoxSlider extends StatelessWidget {
             height: 120,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: makeBoxImages(movies),
+              children: makeBoxImages(context, movies),
             ),
           ),
         ],
@@ -30,12 +31,23 @@ class BoxSlider extends StatelessWidget {
 }
 
 //TODO: 사각형 이미지를 만들어주는 함수
-List<Widget> makeBoxImages(List<Movie> movies) {
+List<Widget> makeBoxImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
   for (var i = 0; i < movies.length; i++) {
     results.add(
       InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<Null>(
+              fullscreenDialog: true,
+              builder: (BuildContext context) {
+                return DetailScreen(
+                  movie: movies[i],
+                );
+              },
+            ),
+          );
+        },
         child: Container(
           padding: EdgeInsets.only(right: 10),
           child: Align(
